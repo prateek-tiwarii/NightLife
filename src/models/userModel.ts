70e8edafc36e5c58fa3c7385058e537e945945ec
Keshav <keshav2021csds116@abesit.edu.in>
2024-10-19 01:38:40 +0530

    fix

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -13,6 +13,6 @@ const userSchema =  new mongoose.Schema({
  });
 
 
- const User =  mongoose.models.User || mongoose.model('User' , userSchema);
+ const User =  mongoose.models?.User || mongoose.model('User' , userSchema);
 
  export default User;
